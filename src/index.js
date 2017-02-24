2d1567b7896f759c89753350a28d834be4829e75
Jim Hoy <jim.hoy@gmail.com>
2017-02-23 19:01:35 -0500

    incremental progress...

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory} from 'react-router';
 import routes from './routes';
-import './styles/site.css'
+import './styles/site.css';
 
 render(
     <Router history={browserHistory} routes={routes}/>,
